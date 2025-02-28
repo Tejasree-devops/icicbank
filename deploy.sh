a9d025d7c6f701ddf2812fb4398d7fb3a05996b5
Tejasree123235 <sree60885@gmail.com>
2025-02-28 05:59:47 +0000

    udated the deploy and create the python

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,30 @@
  hello 
  iam learning about github
  i will definetly learn about git hub perfectly
+
+ .....
+ yes iam learning git commnds the commands are
+ git init
+ git status[working area,stagging area,local repositary]
+ once you have to execute a file it will be in working area
+ you have to go that fle in stagging arae means use gir add .
+ beffore you have to commit do one thing you config the file
+ git config --global user.name [username]
+ git config --global user.email [useremail]
+ now youhave to check the two is there 
+ git config --global --list
+ you have to commit 
+ git commit -m "message" if you want to perticilar file you have to menction that name
+the file in local repositary now you have to push the file in remote repositary now you can use the
+	git remote add orgin[aliyas]name [uel]http or ssh
+	once you have to check the means 
+	git remote -v
+
+now you have to check push the files in remote repositary 
+git push orgin name branch name [by default branch name is master]
+now you have to go and check the github the files are there
+git clean
+ fit reset
+ git revert [commitid]
+ ater you have to push
+ ask the username and passwrd the password is pat password
